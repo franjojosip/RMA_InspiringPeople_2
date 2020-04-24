f0aa8f89f5e4a5db8d49f368440733dad6361893
franjojosip <franjojosip.jukic2@gmail.com>
2020-04-25 00:02:17 +0200

    Remove redundant classes

--- app/src/main/java/ht/ferit/fjjukic/rma_lv2_2/FragmentListener.kt
+++
@@ -1,6 +0,0 @@
-package ht.ferit.fjjukic.rma_lv2_2
-
-interface FragmentListener {
-    fun backAction()
-    fun addNewItem()
-}
--- app/src/main/java/ht/ferit/fjjukic/rma_lv2_2/InspiringPeopleListener.kt
+++
@@ -1,7 +0,0 @@
-package ht.ferit.fjjukic.rma_lv2_2
-
-interface InspiringPeopleListener {
-    fun onShowQuote(index: Int)
-    fun checkRemoveItem(index: Int)
-    fun editItem(index: Int)
-}
